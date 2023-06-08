fd03540d4abbe56c78615b4149c5ed9092290fdc
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2023-06-08 09:13:02 +0100

    Create QuickAssetAction.h

---
+++ LongTools/Source/Public/AssetActions/QuickAssetAction.h
@@ -0,0 +1,41 @@
+// copyright longPointer* 2023
+
+#pragma once
+
+// -----------------------------------------------------------------------------
+
+#include "CoreMinimal.h"
+#include <Editor\Blutility\Classes\AssetActionUtility.h>
+
+#include "QuickAssetAction.generated.h"
+
+// -----------------------------------------------------------------------------
+
+UCLASS()
+class LONGTOOLS_API UQuickAssetAction : public UAssetActionUtility
+{
+	GENERATED_BODY()
+
+public:
+	UQuickAssetAction();
+
+	UFUNCTION(CallInEditor)
+	void TestFunction();
+
+	UFUNCTION(CallInEditor)
+	void DuplicateAssets(int32 pNumOfDuplicates);
+
+	UFUNCTION(CallInEditor)
+	void AutoPrefix();
+
+	UFUNCTION(CallInEditor)
+	void RemoveUnusedAssets();
+
+private:
+	TMap<UClass*, FString> mPrefixMap;
+	
+};
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
